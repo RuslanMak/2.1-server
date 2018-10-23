6b45d926db0d92649be6d4a2553f8e4fda571612
RuslanMak <makarovemd@gmail.com>
2018-10-23 21:16:40 +0300

    done

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+$json = file_get_contents(__DIR__ . '/data.json');
+$contacts = json_decode($json, true);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>JSON</title>
+  <style>
+    table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+      padding: 5px;
+    }
+  </style>
+</head>
+<body>
+ 
+  <table>
+    <tr>
+      <th>Firstname</th>
+      <th>Lastname</th>
+      <th>Address</th>
+      <th>Phone</th>
+    </tr>
+    
+    <?php foreach($contacts as $contact) : ?>
+      <tr>
+        <td><?php echo $contact['firstName']?></td>
+        <td><?php echo $contact['lastName']?></td>
+        <td><?php echo $contact['address']['city'] . ' ' . $contact['address']['street']?></td>
+        <td>
+          <?php
+            foreach($contact['phoneNumber'] as $phone) {
+              echo $phone . '<br>';
+            }
+          ?>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+    
+  </table>
+  
+</body>
+</html>
